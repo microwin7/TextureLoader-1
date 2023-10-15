f28df9efda1791ba160cffb103ebb63d1f98e12c
Gravita <12893402+gravit0@users.noreply.github.com>
2023-10-16 00:02:09 +0700

    [FIX] Sample config file

--- config/Config.php
+++ config/Config.php
@@ -7,10 +7,10 @@ class Config
     public static $dbsystem = "pgsql"; //mysql or pgsql
     public static $port = "5432"; // default mysql port 3306 | default pgsql port 5432
     public static $host = 'localhost'; // database host
-    public static $db = 'launchserver'; // database name
-    public static $user = 'launchserver'; // user name
-    public static $password = '12345'; // password
-    public static $baseUrl = "http://test.gravita.local/my/public/assets/";
+    public static $db = 'db'; // database name
+    public static $user = 'user'; // user name
+    public static $password = 'password'; // password
+    public static $baseUrl = "http://example.com/assets/";
     public static $baseDir = "assets/";
 
     public static $maxUploadWidth = 64;
