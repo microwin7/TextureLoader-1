cc95c4115f6b4d6436587b941e418500a953ea20
Gravita <12893402+gravit0@users.noreply.github.com>
2023-10-16 13:35:02 +0700

    Update readme, remove debug

--- public/get.php
+++ public/get.php
@@ -4,9 +4,9 @@ use Gravita\JsonTextureProvider\Base;
 use Gravita\JsonTextureProvider\Config\Config;
 use function Gravita\JsonTextureProvider\json_response;
 
-ini_set('error_reporting', E_ALL); // FULL DEBUG 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1); // !!! Потом убрать
+// ini_set('error_reporting', E_ALL); // FULL DEBUG 
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
 
 require_once(__DIR__ . '/../vendor/autoload.php');
 
--- public/upload.php
+++ public/upload.php
@@ -6,9 +6,9 @@ use function Gravita\JsonTextureProvider\json_response;
 use function Gravita\JsonTextureProvider\get_bearer_token;
 use function Gravita\JsonTextureProvider\parse_public_key;
 
-ini_set('error_reporting', E_ALL); // FULL DEBUG 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1); // !!! Потом убрать
+// ini_set('error_reporting', E_ALL); // FULL DEBUG 
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
 
 require_once(__DIR__ . '/../vendor/autoload.php');
 
