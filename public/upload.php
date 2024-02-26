e2be3df7929815b0f0f3edc43287d09e94a880ae
Gravita <12893402+gravit0@users.noreply.github.com>
2024-02-26 16:03:14 +0700

    Implement AVATAR

--- public/upload.php
+++ public/upload.php
@@ -7,9 +7,9 @@ use function Gravita\JsonTextureProvider\json_response;
 use function Gravita\JsonTextureProvider\get_bearer_token;
 use function Gravita\JsonTextureProvider\parse_public_key;
 
-// ini_set('error_reporting', E_ALL); // FULL DEBUG 
-// ini_set('display_errors', 1);
-// ini_set('display_startup_errors', 1);
+ini_set('error_reporting', E_ALL); // FULL DEBUG 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 
 require_once(__DIR__ . '/../vendor/autoload.php');
 
@@ -112,16 +112,33 @@ if ($width > Config::$maxUploadWidth) {
 $hash = hash("sha256", $content);
 $filePath = Config::$baseDir . $hash . ".png";
 
+$pdo = $base->pdo;
+$dao = new DAO($pdo, Config::$dbsystem);
 if (!file_exists($filePath)) {
-    file_put_contents($filePath, $content);
+    if($assetType == 'SKIN' && Config::$generateAvatar) {
+        $oldAvatar = $dao->getByUserUuid($uuid, "AVATAR");
+        file_put_contents($filePath, $content);
+        if(!$oldAvatar) {
+            $scale = $width / 64;
+            $skinSize = $scale * 8;
+            $image = imagecreatefromstring($content);
+            $newImage = imagecreatetruecolor($skinSize, $skinSize);
+            imagecopyresized($newImage, $image, 0, 0, $skinSize, $skinSize, $skinSize, $skinSize, $skinSize, $skinSize);
+            imagecopyresized($newImage, $image, 0, 0, $skinSize, $skinSize, 5*$skinSize, $skinSize, $skinSize, $skinSize);
+            imagepng($newImage, $fileinfo['tmp_name']);
+            imagedestroy($image);
+            imagedestroy($newImage);
+            $avatarContent = file_get_contents($fileinfo['tmp_name']);
+            $avatarHash = hash("sha256", $avatarContent);
+            $dao->update($uuid, "AVATAR", $avatarHash, "{}");
+        }
+    }
 }
 $metadata = [];
 if ($assetType == "SKIN" && $options["modelSlim"] == true) {
     $metadata["model"] = "slim";
 }
 $metadata_json = json_encode($metadata);
-$pdo = $base->pdo;
-$dao = new DAO($pdo, Config::$dbsystem);
 $dao->update($uuid, $assetType, $hash, $metadata_json);
 json_response(200, [
     "url" => Config::$baseUrl . $hash . ".png",
